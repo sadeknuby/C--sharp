1e46025edf56b866c2cf385ba52a8e2571563c26
Sadek Mohammad <sadeknuby@gmail.com>
2025-10-20 21:25:01 +0300

    add day 2

---
+++ day2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace program;
+public static class program
+{
+    public static void Main()
+    {
+        int x;
+        x = 7;
+        int y = 12;
+        int z = x + y;
+        char char1= 's';
+        char char2 = 'a';
+        char char3 = 'd';
+        char char4 = 'e';
+        char char5 = 'k';
+        string lastname = " mohammad";
+        Console.WriteLine("my name is: " char1 + char2 + char3 + char4 + char5 + lastname);
+
+        Console.WriteLine("\n age:" +z);
+
+
+
+
+    }
+
+}

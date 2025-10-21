f474f01b8f94352c150258df5d28dc55fc943aa1
Sadek Mohammad <sadeknuby@gmail.com>
2025-10-21 19:28:04 +0300

    add day 3

---
+++ day 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class program
+{
+    static void Main(string[]args)
+    {
+    string name = "sadek";
+    string age = "19" ;
+    string country = "Egypt";
+        Console.Write("Name:"+name);
+        Console.WriteLine("\nAge:"+age);
+        Console.WriteLine("Country:"+country);
+        Console.Write("\nHello, My Name Is " + name + " And iam " + age + " years old and i live in" + country);
+
+
+
+
+
+
+
+
+
+
+}}
+
+

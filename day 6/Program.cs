1d329b73dc5532b16b67c4e303ad1c25348266a0
Sadek Mohammad <sadeknuby@gmail.com>
2025-10-26 17:28:14 +0300

    add day 6

---
+++ day 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace app
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Radius:");
+            float radius = Single.Parse(Console.ReadLine());
+            const float pi = 3.14f;
+            float ball_area = 4 * radius * radius * pi;
+            Console.WriteLine($"The Ball Area Is:{ball_area}");
+
+
+
+
+
+
+        }
+    }
+}

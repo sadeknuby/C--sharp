4b7e137e6fe734d109b55ca7e0548b90e3f08b07
Sadek Mohammad <sadeknuby@gmail.com>
2025-10-28 16:18:33 +0300

    add day 8

---
+++ day 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+namespace app
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+
+            string r = "*";
+            while(r !="*****")
+            {
+                Console.WriteLine(r);
+                r = r +"*";
+
+
+
+            }
+
+
+
+            char a = '@';
+            for (int i = 0; i <26 ; i++)
+            {
+                a += '';
+                Console.Write(a+" ");
+            }
+
+        }
+    }
+}
